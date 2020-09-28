05c6e2ad6b6b749e7f610ead5fb8ecbdf6a516c4
chiru2000 <chiranthangadi123@gmail.com>
2020-09-28 17:20:29 +0530

    colt-ify things

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/colt_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_phoenix-userdebug \
-    lineage_phoenix-eng
+    colt_phoenix-userdebug \
+    colt_phoenix-eng
